2af112aeb0cda11858595e2c809459fa95ba87fb
duanww <duanww@fxiaoke.com>
2018-08-07 15:18:47 +0800

    Initial Commit

---
+++ OnePiece/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  OnePiece
+//
+//  Created by Duanww on 2018/8/7.
+//  Copyright © 2018年 Duanww. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ OnePiece/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  OnePiece
+//
+//  Created by Duanww on 2018/8/7.
+//  Copyright © 2018年 Duanww. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
